9c345d79c1aab469f0e76aaee2dd554d8929a2c6
Theodor Diaconu <diaconu.theodor@gmail.com>
2021-07-01 09:23:34 +0300

    commented out warning, for now

--- src/di.ts
+++ src/di.ts
@@ -14,9 +14,9 @@ export class ContainerInstance extends BaseContainerInstance {
       // The reason we do this is to allow services that don't specify @Service()
       if (e instanceof ServiceNotFoundError) {
         if (typeof id === "function") {
-          console.warn(
-            `You have tried to get from the container a class (${id?.name}) which doesn't have @Service() specified. Please add it to remove this warning.`
-          );
+          // console.warn(
+          //   `You have tried to get from the container a class (${id?.name}) which doesn't have @Service() specified. Please add it to remove this warning.`
+          // );
           this.set({
             id: id as Function,
             type: id as any,
