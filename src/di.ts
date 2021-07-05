a4d55649596e36598ad6c66018c555a282e5cfd6
Theodor Diaconu <diaconu.theodor@gmail.com>
2021-07-05 18:55:20 +0300

    (fix) typedi 10, ui fix

--- src/di.ts
+++ src/di.ts
@@ -12,7 +12,10 @@ export class ContainerInstance extends BaseContainerInstance {
       return super.get(id);
     } catch (e) {
       // The reason we do this is to allow services that don't specify @Service()
-      if (e instanceof ServiceNotFoundError) {
+      if (
+        e instanceof ServiceNotFoundError ||
+        e.toString() === "ServiceNotFoundError"
+      ) {
         if (typeof id === "function") {
           // console.warn(
           //   `You have tried to get from the container a class (${id?.name}) which doesn't have @Service() specified. Please add it to remove this warning.`
